b61aa1ae1e9beac744ac615e0989220ff8e3b844
Cristian Maglie <c.maglie@bcmi-labs.cc>
2017-08-25 15:08:18 +0200

    fixed licensing info

--- properties_test.go
+++ properties_test.go
@@ -1,9 +1,9 @@
 /*
  * This file is part of PropertiesMap library.
  *
- * Copyright 2017 Arduino LLC (http://www.arduino.cc/)
+ * Copyright 2017 Arduino AG (http://www.arduino.cc/)
  *
- * Arduino Builder is free software; you can redistribute it and/or modify
+ * PropertiesMap library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
